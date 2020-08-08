a923f438572d13e0ec0df8f2b653db872762f7f6
ApiTrader <printingpresstrader.one@gmail.com>
2020-08-07 19:52:08 -0700

    Adding first test

---
+++ bybit_sdk_pytest/test_public_endpoints.py
@@ -0,0 +1,7 @@
+import pytest
+from bybit_sdk.bybit_client import ByBitClient
+
+def test_server_time():
+    client = ByBitClient()
+    res = client.get_server_time()
+    assert res.get('ret_code') == 0
